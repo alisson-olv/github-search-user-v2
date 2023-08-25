cf3feef72657c664de958cb21055f4db5d99331d
Alisson Oliveira <binho_alisson@hotmail.com>
2023-08-25 20:29:33 -0300

    refactor: testando em prod se o navigate do fetch está funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Profile from "./pages/Profile"
 import UserContext from "./context/UserContext"
+import NotFound from "./pages/NotFound"
 
 
 function App() {
@@ -12,10 +13,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/404" element={<NotFound />} />
         </Routes>
       </UserContext>
     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -4,10 +4,13 @@ import { useNavigate } from "react-router-dom";
 export const useFetch = (user) => {
   const [data, setData] = useState(null);
   const [userRepos, setUserRepos] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
   useEffect(() => {
+
     if (user !== null) {
       const fetchData = async () => {
         const response = await fetch(`https://api.github.com/users/${user}`);
@@ -25,7 +28,7 @@ export const useFetch = (user) => {
 
       fetchRepos();
 
-    } else if (user == null) {
+    } else {
       navigate('/');
     }
 
