6b7fdd9627d2e2b6b48e9b61979edd8b0c80989d
Alisson Oliveira <binho_alisson@hotmail.com>
2023-08-25 21:34:15 -0300

    update: remover gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/github-search-user-v2"
+  plugins: [react()]
 })
